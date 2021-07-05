83bb2e54f6eaaae53dbc5da66e96eae8557af06c
NicolasKozak <nicolas.kozak5@gmail.com>
2021-07-05 09:27:47 +0200

    instrucciones para el futuro

--- code.R
+++ code.R
@@ -5,3 +5,11 @@ print("holaa de nuevo")
 print("nuevo dia")
 print("he hecho lo de cambiar la url")
 print("parece que funciona, esto está subido sin meter contraseña")
+
+print("Para el futuro, he usado:")
+
+#para cambiar a ssh, desde github click en clonar y copiar el link ssh:
+print("git remote set-url origin git@github.com:<Username>/<Project>.git")
+
+#despues de hacer el cambio a ssh, hacer el primer push desde el shell
+print("git push origin")
